4518898e51876bce2fa60d2cb99f56ecc1e0f159
Mr. King <cking@sjusd.org>
2020-01-18 14:54:11 -0800

    Initial commit, base code

---
+++ src/main/include/Robot.h
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+
+#include <frc/Joystick.h>
+#include <ctre/Phoenix.h>
+
+class Robot : public frc::TimedRobot {
+ public: 
+
+  TalonSRX* talon;
+  
+  frc::Joystick* joy;
+
+
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void TestPeriodic() override;
+  
+ private:
+
+};
